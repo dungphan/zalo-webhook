3ea07d9ac0d9a25c743c9e80a1aeb870a7de1190
Phan Viet Dung <dung.phanv@gmail.com>
2019-12-31 09:12:52 +0700

    - update get event

--- app.js
+++ app.js
@@ -41,7 +41,10 @@ app.get('/', function (req, res) {
    res.send('hello world');
 });
 
-app.get('/zalotoken', function (req, res) {
+///
+///
+///
+app.get('/api/zalo/token', function (req, res) {
    //res.header("Access-Control-Allow-Origin", "*");
    if (req.query.access_token && req.quey.oaId){
       zaloAppToken = req.query.access_token;
@@ -50,9 +53,9 @@ app.get('/zalotoken', function (req, res) {
    res.status(200).end();
 });
 
-app.get('/get-zalotoken', function (req, res) {
+app.get('/api/zalo/gettoken', function (req, res) {
    //res.header("Access-Control-Allow-Origin", "*");
-   var url = 'https://oauth.zaloapp.com/v3/oa/permission?app_id='+zaloAppId+'&redirect_uri='+'http://' + req.headers['host'] + '/zalotoken';
+   var url = 'https://oauth.zaloapp.com/v3/oa/permission?app_id='+zaloAppId+'&redirect_uri='+'http://' + req.headers['host'] + '/api/zalo/token';
    request(url,function(err, response, body){
       if (err)
          console.log(err);
@@ -61,6 +64,97 @@ app.get('/get-zalotoken', function (req, res) {
    res.status(200).end();
 });
 
+router.post('/api/zalo/events', function (req, res, next) {
+   var body = JSON.parse(req.body);
+   switch (body.event_name) {
+   case user_send_text: {
+         if (body.message.text.startWiths('hello')){
+            // say hi
+            res.status(200).end();
+            // get user data
+            var userid = body.sender.id;
+            var url = 'https://openapi.zalo.me/v2.0/oa/getprofile?access_token='+zaloAppToken+'&data={"user_id":"'+userid+'"}';
+            request(url, function(err,response,body){
+               if (err)
+                  break;
+               var username = body.data.display_name;
+               // message back to user
+               var url = 'https://openapi.zalo.me/v2.0/oa/message?access_token='+zaloAppToken;
+               var form = {
+                  "recipient": {
+                     "user_id": userid
+                  },
+                  "message": ("Hello "+ username+"!")
+               };
+               request.post(url, form: form, function(err,response,body){
+                  if (err){
+                     console.log(err);
+                     break;
+                  }
+                  
+               });
+            });
+         }
+         else if (body.message.text.startWiths('checkin')){
+            
+         }
+         break;
+      }
+   case user_received_message: {
+
+         break;
+      }
+   case user_seen_message: {
+
+         break;
+      }
+   case follow: {
+
+         break;
+      }
+   case oa_send_text: {
+
+         break;
+      }
+   case oa_send_image: {
+
+         break;
+      }
+   case oa_send_list: {
+
+         break;
+      }
+   default:
+      break;
+   }
+   return res.status(200).end();
+});
+
+// - /api/zalo/messagetouser - Params: userid, message
+router.post('/api/zalo/messagetouser', function (req, res, next) {
+   var body = {
+      "recipient": {
+         "user_id": req.body.userid
+      },
+      "message": req.body.message
+   };
+   //request.post({url:'http://service.com/upload', form: {key:'value'}}, function(err,httpResponse,body){ /* ... */ });
+});
+
+// - /api/zalo/broadcasttouser - Params: target, message
+router.post('/api/zalo/broadcasttouser', function (req, res, next) {
+   var body = {
+      "recipient": {
+         "target": req.body.target
+      },
+      "message": req.body.message
+   };
+   //request.post({url:'http://service.com/upload', form: {key:'value'}}, function(err,httpResponse,body){ /* ... */ });
+   
+});
+
+///
+///
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
